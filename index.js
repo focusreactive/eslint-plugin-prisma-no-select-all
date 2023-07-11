2bc7db893858136f717ac61ef414fd9170924292
PixelsCommander <denis.radin@gmail.com>
2023-07-11 12:27:09 +0200

    Adds metadata to index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
-    name: "eslint-plugin-prisma-no-select-all",
-    version: "1.0.0",
+    meta: {
+        name: "eslint-plugin-prisma-no-select-all",
+        version: "1.0.0",
+    },
     rules: {
         'prisma-no-select-all': require('./prisma-no-select-all'),
     }
--- prisma-no-select-all.js
+++ prisma-no-select-all.js
@@ -10,7 +10,7 @@ module.exports = {
                 const propertyName = node?.callee?.property?.name || null;
                 if (propertyName === 'findUnique' || propertyName === 'findMany' || propertyName === 'findFirst') {
                     const args = node?.arguments[0]?.properties || [];
-                    const select = args.find((arg) => arg.key.name === 'select');
+                    const select = args.find((arg) => arg?.key?.name === 'select');
                     if (!select) {
                         context.report({
                             node,
